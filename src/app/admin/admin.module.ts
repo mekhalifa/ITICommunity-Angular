41ae92c971d2e4390ac68ba57726f0cb8e0e6317
Mohamed Emad Khalifa <m_e_khalifa@hotmail.com>
2020-07-09 19:08:37 +0200

    iti project

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminComponent } from './admin/admin.component';
+import { AdminComponent } from './admin.component';
 
 
 @NgModule({
